805006546852d9820e9041ee87d4ec1531c26e52
Nurbain <nathan.urbain@etu.unistra.fr>
2017-03-07 00:15:32 +0100

    Fonction basic de marche de la Simulation

--- Projet_IHM/Simulation/mainwindow.cpp
+++ Projet_IHM/Simulation/mainwindow.cpp
@@ -1,14 +1,128 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    //Init
+    EauSaS = ui->EauSas;
+    BateauAmont = ui->BateauAmont;
+    BateauAvale = ui->BateauAvale;
+    InitUi();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::InitUi()
+{
+    //Init Place
+    BateauAmont->setGeometry(120,30,91,81);
+    BateauAvale->setGeometry(540,110,91,81);
+
+    //Cache
+    BateauAmont->setVisible(false);
+    BateauAvale->setVisible(false);
+}
+
+void MainWindow::OuverturePorte(QGraphicsView* porte){
+
+    QRect geo_new = porte->geometry();
+
+    geo_new.translate(0,-15);
+    porte->setGeometry(geo_new);
+}
+
+void MainWindow::FermeturePorte(QGraphicsView* porte){
+
+    QRect geo_new = porte->geometry();
+
+    geo_new.translate(0,15);
+    porte->setGeometry(geo_new);
+}
+
+void MainWindow::EauSASmonte()
+{
+     QRect geo_new = EauSaS->geometry();
+
+    //QRect geo_bateau = bateau->geometry();
+     //scale
+     geo_new.setHeight(geo_new.height()+10);
+
+     //geo_bateau.translate(0,-10);
+     //translation parce que scale par rapport a en haut
+     geo_new.translate(0,-10);
+     EauSaS->setGeometry(geo_new);
+}
+
+void MainWindow::EauSASdescend()
+{
+     QRect geo_new = EauSaS->geometry();
+
+     //QRect geo_bateau = bateau->geometry();
+
+     //scale
+     geo_new.setHeight(geo_new.height()-10);
+
+     //geo_bateau.translate(0,+10);
+     //translation parce que scale par rapport a en haut
+     geo_new.translate(0,+10);
+     EauSaS->setGeometry(geo_new);
+}
+
+void MainWindow::BateauAvanceAmont()
+{
+    QRect new_geo = BateauAmont->geometry();
+    new_geo.translate(10,0);
+    BateauAmont->setGeometry(new_geo);
+}
+
+void MainWindow::BateauAvanceAvale()
+{
+    QRect new_geo = BateauAvale->geometry();
+    new_geo.translate(-10,0);
+    BateauAvale->setGeometry(new_geo);
+}
+
+
+//Bouton Test
+
+void MainWindow::on_Etap1_clicked()
+{
+    OuverturePorte(ui->Porte1);
+}
+
+void MainWindow::on_Etape1_clicked()
+{
+    OuverturePorte(ui->Porte2);
+}
+
+void MainWindow::on_Etap2_clicked()
+{
+    EauSASmonte();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    EauSASdescend();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    BateauAvale->setVisible(true);
+    BateauAvanceAvale();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    BateauAmont->setVisible(true);
+    BateauAvanceAmont();
 }
--- Projet_IHM/Simulation/mainwindow.h
+++ Projet_IHM/Simulation/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "ui_mainwindow.h"
+#include "mainwindow.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +19,27 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QGraphicsView* EauSaS;
+    QLabel* BateauAmont;
+    QLabel* BateauAvale;
+
+public slots:
+    void OuverturePorte (QGraphicsView *porte);
+    void FermeturePorte(QGraphicsView* porte);
+    void EauSASmonte();
+    void EauSASdescend();
+    void InitUi();
+    void BateauAvanceAmont();
+    void BateauAvanceAvale();
+
+    private slots:
+    void on_Etap1_clicked();
+    void on_Etape1_clicked();
+    void on_Etap2_clicked();
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
 };
 
 #endif // MAINWINDOW_H
+
